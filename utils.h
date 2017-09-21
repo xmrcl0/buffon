8c7d254f2f249e44827305d98c517c6acd00d017
Marcelo Pinto <marcelo@mt4.com.br>
2017-09-21 14:29:20 -0300

    refactor: Troca do nome das funções
    As funções foram renomeadas para melhorar o entendimento de sua
    finalidade.

--- utils.h
+++ utils.h
@@ -2,12 +2,14 @@
  *  @brief    Function prototypes for utils.
  *  @author   Marcelo Pinto (xmrcl0@gmail.com)
  *  @date     09/12/2017
+ *  @update   09/21/2017
  */
 
 #include <regex.h>
 #include <stdio.h>
 
-/** @brief Verify if a string is a valid integer.
+
+/** @brief Verify if a string is a valid natural number.
  *
  *  @param[in] n  Number to be validated
  *  @return Validation status 
@@ -16,10 +18,10 @@
  *  @retval  1 Failure
  *  @retval -1 Internal error
  */
-int is_integer(char *n);
+int is_natural_num(char *n);
 
 
-/** @brief Verify if a string is a valid number.
+/** @brief Verify if a string is a valid positive number.
  *
  *  @param[in] number Number to be validated
  *  @return Validation status 
@@ -28,4 +30,4 @@ int is_integer(char *n);
  *  @retval  1 Failure
  *  @retval -1 Internal error
  */
-int is_positive_number(char *n);
+int is_positive_num(char *n);
